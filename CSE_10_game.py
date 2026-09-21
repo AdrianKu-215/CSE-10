1f015e19fbabb67377c4dd13ba6760a3e2b26f22
AdrianKu-215 <adrian.ku@student.cisgz.com>
2026-09-21 10:43:08 +0800

    debugged game

--- CSE_10_game.py
+++ CSE_10_game.py
@@ -1,98 +1,100 @@
-#newwork
 import random
 import sys
 import time
+
 def slow(text):
     print(text)
     time.sleep(1.2)
+
 health = 1000
 attack = 100
-coins =  random.randint(100,150)
-monster1 = False
-strongmonster = False
-toll = 0
-potion = random.randint(1,99)
-sword = int(attack + 1000)
-demonking = 0
-shop = 0
+coins = random.randint(100, 150)
+potions = random.randint(1, 99)
 sword = False
-potion = False
 
-if health == 0:
-    slow("you've ran out of health, better luck next time")
-    sys.exit()
+def check_death():
+    global health
+    if health <= 0:
+        slow("You've run out of health, better luck next time!")
+        sys.exit()
 
 name = input("Welcome traveler, you are now in a magical land. Your objective is to defeat the demon king, what is your name : ")
-print("Alright " ,name, " your journey begins now, you have ",health," health, ",attack," attack power, and ",coins," coins.")
-choice1 = input("You encounter a monster, do you want to fight it or run away? (fight/run) : ")
-if name == "adrian":
+print("Alright ", name, " your journey begins now, you have ", health, " health, ", attack, " attack power, and ", coins, " coins.")
+
+if name.lower() == "adrian":
     health += 10000
     attack += 10000
     coins += 10000
     slow("You have been given a special boost for being the creator of this game! You now have " + str(health) + " health, " + str(attack) + " attack power, and " + str(coins) + " coins.")
-    
+
+# ---- Monster 1 ----
+choice1 = input("You encounter a monster, do you want to fight it or run away? (fight/run) : ")
 if choice1.lower() == "fight":
-    monster1 = True
     slow("You have chosen to fight the monster!")
     slow("The monster has 500 health and 50 attack power.")
-    while monster1:
+    monster_health = 500
+    while monster_health > 0:
         choice2 = input("Do you want to attack or use a potion? (attack/potion) : ")
         if choice2.lower() == "attack":
             slow("You attack the monster!")
-            monster_health = 500 - 100
+            monster_health -= attack
+            if monster_health <= 0:
+                slow("You have defeated the monster!")
+                coins += random.randint(10, 50)
+                slow("You have gained some coins! You now have " + str(coins) + " coins.")
+                break
             slow("The monster now has " + str(monster_health) + " health left.")
             slow("The monster attacks you back!")
             health -= 50
             slow("You now have " + str(health) + " health left.")
-            if monster_health <= 0:
-                slow("You have defeated the monster!")
-                coins += random.randint(10,50)
-                slow("You have gained some coins! You now have " + str(coins) + " coins.")
-                monster1 = False
+            check_death()
         elif choice2.lower() == "potion":
-            if potion > 0:
+            if potions > 0:
                 health += 100
-                potion -= 1
+                potions -= 1
                 slow("You used a potion and gained 100 health! You now have " + str(health) + " health left.")
             else:
                 slow("You don't have any potions left!")
         else:
             slow("Invalid choice, please choose again.")
-if choice1.lower() == "run":
+else:
     slow("You have chosen to run away from the monster.")
 
-choicestrongmonster = input("You encounter a strong monster, do you want to fight it or run away? (fight/run) : ")
-if choicestrongmonster.lower() == "fight":
-    strongmonster = True
+# ---- Strong monster ----
+choice_strong = input("You encounter a strong monster, do you want to fight it or run away? (fight/run) : ")
+if choice_strong.lower() == "fight":
     slow("You have chosen to fight the strong monster!")
     slow("The strong monster has 1000 health and 100 attack power.")
-    while strongmonster:
+    strong_health = 1000
+    while strong_health > 0:
         choice3 = input("Do you want to attack or use a potion? (attack/potion) : ")
         if choice3.lower() == "attack":
             slow("You attack the strong monster!")
-            strong_monster_health = 1000 - attack
-            slow("The strong monster now has " + str(strong_monster_health) + " health left.")
+            strong_health -= attack
+            if strong_health <= 0:
+                slow("You have defeated the strong monster!")
+                coins += random.randint(50, 100)
+                slow("You have gained some coins! You now have " + str(coins) + " coins.")
+                break
+            slow("The strong monster now has " + str(strong_health) + " health left.")
             slow("The strong monster attacks you back!")
             health -= 100
             slow("You now have " + str(health) + " health left.")
-            if strong_monster_health <= 0:
-                slow("You have defeated the strong monster!")
-                coins += random.randint(50,100)
-                slow("You have gained some coins! You now have " + str(coins) + " coins.")
-                strongmonster = False
+            check_death()
         elif choice3.lower() == "potion":
-            if potion > 0:
+            if potions > 0:
                 health += 100
-                potion -= 1
+                potions -= 1
                 slow("You used a potion and gained 100 health! You now have " + str(health) + " health left.")
             else:
                 slow("You don't have any potions left!")
         else:
             slow("Invalid choice, please choose again.")
-if choicestrongmonster.lower() == "run":
+else:
     slow("You have chosen to run away from the strong monster.")
 
-toll = random.randint(101,125)
+# ---- Toll bridge ----
+toll = random.randint(101, 125)
 slow("You encounter a toll bridge, you need to pay " + str(toll) + " coins to cross it.")
 if coins >= toll:
     coins -= toll
@@ -101,15 +103,16 @@ else:
     slow("You don't have enough coins to pay the toll. You cannot cross the bridge.")
     sys.exit()
 
-shop = input("You encounter a shop, do you want to buy a potion for 50 coins or a sword for 100 coins? (potion/sword/none) : ")
-if shop.lower() == "potion":
+# ---- Shop ----
+shop_choice = input("You encounter a shop, do you want to buy a potion for 50 coins or a sword for 100 coins? (potion/sword/none) : ")
+if shop_choice.lower() == "potion":
     if coins >= 50:
         coins -= 50
-        health += 500
-        slow("You have bought a potion! You now have " + str(potion) + " potions and " + str(coins) + " coins left.")
+        potions += 1
+        slow("You have bought a potion! You now have " + str(potions) + " potions and " + str(coins) + " coins left.")
     else:
         slow("You don't have enough coins to buy a potion.")
-elif shop.lower() == "sword":   
+elif shop_choice.lower() == "sword":
     if coins >= 100:
         coins -= 100
         attack += 1000
@@ -117,38 +120,36 @@ elif shop.lower() == "sword":
         slow("You have bought a sword! Your attack power is now " + str(attack) + " and you have " + str(coins) + " coins left.")
     else:
         slow("You don't have enough coins to buy a sword.")
-elif shop.lower() == "none":
+else:
     slow("You have chosen not to buy anything from the shop.")
 
-if sword == True:
-    slow("You have a sword, your attack power is now " + str(attack) + ".")
-if potion == True:
-    slow("You have a potion, your health is now " + str(health) + ".")
-
-demonking = input("You encounter the demon king, do you want to fight it or run away? (fight/run) : ")
-if demonking.lower() == "fight":
+# ---- Demon king ----
+demon_choice = input("You encounter the demon king, do you want to fight it or run away? (fight/run) : ")
+if demon_choice.lower() == "fight":
     slow("You have chosen to fight the demon king!")
     slow("The demon king has 2000 health and 200 attack power.")
-    while True:
+    demon_health = 2000
+    while demon_health > 0:
         choice4 = input("Do you want to attack or use a potion? (attack/potion) : ")
         if choice4.lower() == "attack":
             slow("You attack the demon king!")
-            demon_king_health = 2000 - attack
-            slow("The demon king now has " + str(demon_king_health) + " health left.")
+            demon_health -= attack
+            if demon_health <= 0:
+                slow("You have defeated the demon king! Congratulations, you have completed your journey!")
+                break
+            slow("The demon king now has " + str(demon_health) + " health left.")
             slow("The demon king attacks you back!")
             health -= 200
             slow("You now have " + str(health) + " health left.")
-            if demon_king_health <= 0:
-                slow("You have defeated the demon king! Congratulations, you have completed your journey!")
-                break
+            check_death()
         elif choice4.lower() == "potion":
-            if potion > 0:
+            if potions > 0:
                 health += 100
-                potion -= 1
+                potions -= 1
                 slow("You used a potion and gained 100 health! You now have " + str(health) + " health left.")
             else:
                 slow("You don't have any potions left!")
         else:
             slow("Invalid choice, please choose again.")
-if demonking.lower() == "run":
+else:
     slow("You have failed your journey, better luck next time.")
